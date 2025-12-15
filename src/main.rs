372c2ab2d86e88b4b191a76ba033ab2fa2756bd7
Cadecraft <cadendotlee@gmail.com>
2025-12-15 08:44:04 -0800

    feat: basic axum server

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,37 @@
-use std::convert::Infallible;
-use std::net::SocketAddr;
-
-use http_body_util::Full;
-use hyper::body::Bytes;
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper::{Request, Response};
-use hyper_util::rt::TokioIo;
-use tokio::net::TcpListener;
-
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
-}
+use axum::{
+    routing::{get, put},
+    http::StatusCode,
+    Json, Router,
+};
+use serde::{Deserialize};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // Some code from <https://hyper.rs/guides/1/server/hello-world/>
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let listener = TcpListener::bind(addr).await?;
+async fn main() {
+    let app = Router::new()
+        .route("/data", get(get_data))
+        .route("/data", put(put_data));
 
-    loop {
-        let (stream, _) = listener.accept().await?;
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
 
-        let io = TokioIo::new(stream);
+async fn get_data(Json(payload): Json<GetData>) -> String {
+    format!("Hello world {}", payload.key)
+}
 
-        tokio::task::spawn(async move {
-            if let Err(err) = http1::Builder::new()
-                .serve_connection(io, service_fn(hello))
-                .await
-            {
-                eprintln!("Error serving connection: {:?}", err);
-            }
-        });
-    }
+async fn put_data(Json(payload): Json<PutData>) -> StatusCode {
+    // TODO: impl
+    StatusCode::UNAUTHORIZED
+}
+
+#[derive(Deserialize)]
+struct GetData {
+    key: String
+}
+
+#[derive(Deserialize)]
+struct PutData {
+    key: String,
+    value: String,
+    auth: String,
 }
